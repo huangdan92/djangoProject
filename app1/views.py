800d4718336ee38e4c31a3755fc24529f807a3f5
黄丹 <huangdan05@meituan.com>
2020-08-31 19:39:21 +0800

    first

---
+++ app1/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from app1 import models
+
+
+def say_hello(request):
+    return HttpResponse("helloworld")
+
+
+# 查看数据
+def user_list(request):
+    myalldata = models.Person.objects.all()  # 取出表中所有数据
+    return render(request, 'user_list.html', {'user_list': myalldata})
+
+
+# 新增数据
+def add_user(request):
+    error_name = ''
+    if request.method == 'POST':
+        # 1、获取前端输入的数据
+        name = request.POST.get('name')
+        context = request.POST.get('context')
+        sex = request.POST.get('sex')
+        user_list = models.Person.objects.filter(name=name)
+        # 2、判断数据库是否存在
+        if user_list:
+            error_name = '%s用户名已经存在了' % name
+            return render(request, 'add_user.html', {'error_name': error_name})
+        # 3、存储到数据库中
+        else:
+            user = models.Person.objects.create(name=name, context=context, sex=sex)
+            user.save()
+            return redirect('/user_list/')
+    return render(request, 'add_user.html')
+
+
+# 删除数据
+def delete_user(request):
+    # 取出需要删除的id
+    delete_id = request.GET.get('id')
+    # 从数据库删除的
+    models.Person.objects.filter(id=delete_id).delete()
+    return redirect('/user_list/')
+
+
+# 修改数据
+def update_user(request):
+    update_id = request.GET.get('id')
+    if request.method == 'POST':
+        new_context = request.POST.get('context')
+        # 找到匹配的id
+        update_obj = models.Person.objects.get(id=update_id)
+        # 取出密码和新密码对应
+        update_obj.context = new_context
+        # 保存数据库
+        update_obj.save()
+        return redirect('/user_list/')
+    ret = models.Person.objects.get(id=update_id)
+    return render(request, 'update_user.html', {'ret': ret})
